5ffc10898eea0203b9bbeb7e11d840a124874a99
agres <ale.agresta46@gmail.com>
2022-03-04 16:07:40 +0100

    fix

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\MovieModel;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    $data = MovieModel::all();
+    dump($data);
+    return view('movie.home', compact("data"));
 }
